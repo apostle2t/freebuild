ca682fc6294fba3dc32637ea8c3d72568820d95d
Your Name <your-github-email@example.com>
2026-04-26 00:25:25 +0200

    Webhook set up

---
+++ webhook-project/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+html = """
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title></title>
+    <body></body>
+    </head>
+    </html>
+
+"""
