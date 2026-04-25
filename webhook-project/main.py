f8272016f81cb74a05a01acdc0aa5bcf075011b8
Your Name <your-github-email@example.com>
2026-04-26 00:41:23 +0200

    I am working on the html side of things

--- webhook-project/main.py
+++ webhook-project/main.py
@@ -7,10 +7,19 @@ html = """
 
     <!DOCTYPE html>
     <html>
-    <head>
-    <title></title>
-    <body></body>
-    </head>
+        <head>
+            <title><Chat Application/title>
+        </head>
+        <body>
+            <h1>Welcome to Matthew's Chatting application</h1>
+            <h2>Enter an Input to start chatting<h2>
+            <form action="" onsubmit="connectWebhook(event)">
+                <input type="text" id="inp"/>
+                <button type="submit"/>
+            </form>
+            <ul id="messages"></ul>
+            
+        </body>
     </html>
 
 """
